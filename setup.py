cf0b6349e1021e74b73c288b77d8ebef8f1cc1c3
Mikko Aulamo <aulamo.mikko@gmail.com>
2022-11-11 13:00:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='eflomal',
-    version='0.1.17',
+    version='0.1.18',
     author='Robert Östling',
     url='https://github.com/robertostling/eflomal',
     license='GNU GPLv3',
