c71ce26cdb20792405b20226e73e96e645040efc
Mikko Aulamo <aulamo.mikko@gmail.com>
2022-11-11 11:17:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='eflomal',
-    version='0.1.12',
+    version='0.1.13',
     author='Robert Östling',
     url='https://github.com/robertostling/eflomal',
     license='GNU GPLv3',
@@ -22,6 +22,6 @@ setup(
     install_requires=['numpy', 'Cython'],
     ext_modules=cythonize(cyalign_ext, language_level="3"),
     scripts=['align.py', 'makepriors.py', 'mergefiles.py'],
-    data_files=['eflomal']
+    data_files=[ ('.', ['eflomal']) ]
 )
 
